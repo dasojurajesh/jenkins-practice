a52de7c7283a20dd678bb43b206af960445b2330
“awslinux” <mbmkrishna4602@gmail.com>
2022-10-12 20:19:30 +0530

    added jenkins file

--- stage.groovy
+++ stage.groovy
@@ -1,2 +1,29 @@
-
-
+pipeline {
+    agent any
+    stages{
+        stage ('printing working directory') {
+            steps {
+                sh 'pwd'
+            }
+        }
+        stage ('creating folder') {
+            steps {
+                sh 'mkdir test'
+                sh 'mkdir stage1'
+                sh 'mkdir stage2'
+                sh 'ls -ltrh'
+            }
+        }
+        stage ('delete folder') {
+            steps {
+                sh 'rm -r test'
+            }
+        }
+        stage ('validate') {
+            steps {
+                sh 'ls -ltrh'
+                echo "folder test is deleted "
+            }
+        }
+    }
+}
