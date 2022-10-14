9213cc5fadb5a4b9a6944336387eb6b5a1b48c11
“awslinux” <mbmkrishna4602@gmail.com>
2022-10-14 14:22:19 +0530

    bugfix commit

---
+++ bugfix.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+    stages{
+        stage ('printing working directory') {
+            steps {
+                sh 'pwd'
+
+            }
+
+        }
+        stages ('creating folder') {
+            steps {
+                sh 'mkdir shivani'
+                sh 'ls -ltrh'
+
+            }
+        }
+        stage ('creating files') {
+            steps {
+                sh 'ls ltrh'
+                echo "files copy to the folder"
+                sh 'ls -ltrh'
+            }
+        }
+    }
+}
