1b210310b3a3e5051e4d12a3f07d6767fbaa655e
“awslinux” <mbmkrishna4602@gmail.com>
2022-10-28 14:42:22 +0530

    my first commit

--- docker_jenkins2.groovy
+++ docker_jenkins2.groovy
@@ -22,7 +22,7 @@ pipeline {
         }
         stage ('docker run'){
             steps {
-                sh "docker run -d -p 8080:8083 image14"
+                sh "docker run -d -p 8080:8087 image14"
             }
         }
 
